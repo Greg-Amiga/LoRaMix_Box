0471425a1c217e9f7952437f3ab7a7a47206d4b5
Greg-Amiga <106454790+Greg-Amiga@users.noreply.github.com>
2026-07-17 20:51:59 +0200

    Create Openings.py

---
+++ FreeCAD/core/Openings.py
@@ -0,0 +1,103 @@
+"""
+Création des ouvertures du boîtier
+"""
+
+import Part
+import FreeCAD as App
+
+from core.Geometry import move
+
+
+class OpeningBuilder:
+
+    def __init__(self, body):
+
+        self.body = body
+
+    # --------------------------------------------------
+
+    def rectangular(self,
+                    x,
+                    y,
+                    z,
+                    lx,
+                    ly,
+                    lz):
+
+        hole = Part.makeBox(
+            lx,
+            ly,
+            lz
+        )
+
+        move(
+            hole,
+            x,
+            y,
+            z
+        )
+
+        self.body = self.body.cut(hole)
+
+    # --------------------------------------------------
+
+    def circular(self,
+                 x,
+                 y,
+                 z,
+                 diameter,
+                 depth):
+
+        hole = Part.makeCylinder(
+            diameter/2,
+            depth
+        )
+
+        move(
+            hole,
+            x,
+            y,
+            z
+        )
+
+        self.body = self.body.cut(hole)
+
+    # --------------------------------------------------
+
+    def build(self, board):
+
+        for opening in board["openings"]:
+
+            if opening["type"] == "RJ45":
+
+                self.rectangular(
+                    0,
+                    opening["x"],
+                    opening["z"],
+                    5,
+                    opening["width"],
+                    opening["height"]
+                )
+
+            elif opening["type"] == "BORNIER":
+
+                self.rectangular(
+                    100,
+                    opening["x"],
+                    10,
+                    5,
+                    opening["width"],
+                    opening["height"]
+                )
+
+            elif opening["type"] == "SMA":
+
+                self.circular(
+                    opening["x"],
+                    40,
+                    20,
+                    opening["diameter"],
+                    10
+                )
+
+        return self.body
