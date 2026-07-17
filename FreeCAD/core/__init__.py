8fc42bf1a7c28fbc9e2d6845467548e2a0d36983
Greg-Amiga <106454790+Greg-Amiga@users.noreply.github.com>
2026-07-17 21:24:18 +0200

    Create __init__.py

---
+++ FreeCAD/core/__init__.py
@@ -0,0 +1,9 @@
+"""
+LoRaMix_Box Core Package
+"""
+
+from .Geometry import *
+from .Base import BaseBuilder
+from .Lid import LidBuilder
+from .Openings import OpeningBuilder
+from .Export import ExportManager
